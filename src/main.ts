8539ad76f5f22d10da2e38e608c503e760feb056
T-ArkadiiZelenskyi <T-Arkady.Zelensky@amtwoundcare.com>
2023-12-08 13:36:51 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+
+import * as logger from 'koa-logger';
+import * as json from 'koa-json';
+
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'Hello world11' };
+
+  await next();
+});
+
+app.use(json());
+app.use(logger());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(process.env.PORT, () => {
+  console.log('Started');
+})
